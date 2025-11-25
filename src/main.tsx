20e1be2e26f3df4037dc38fac8340c18df0be038
sofiyankh <sofiyankhalifa.11@gmail.com>
2025-11-25 21:47:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+      <BrowserRouter basename="/SofieneKh">
+      <App />
+    </BrowserRouter>
+  
+  
+  );
   
